17a222275e22e8d50be54ac5176cd02b52533251
gp-kakimoto <kakimoto_t@gp1000.jp>
2025-02-26 16:08:33 +0900

    状態管理まで

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**  @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images:{
+        remotePatterns: [
+            {
+              protocol: 'https',
+              
+              hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL
+              ).hostname,
+            },
+         
+          ]
+        
+    },
+}
 
 module.exports = nextConfig
